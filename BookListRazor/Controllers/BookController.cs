d44ef507b9ec1a7bd84827eb998c2022f15cc76e
TobiFarore <tobifarore@icloud.com>
2021-03-18 15:51:21 +0000

    Fixed 2nd table and is populated as expected

--- BookListRazor/Controllers/BookController.cs
+++ BookListRazor/Controllers/BookController.cs
@@ -25,5 +25,18 @@ namespace BookListRazor.Controllers
         {
             return Json(new { data = await _db.Book.ToListAsync() });
         }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDb = await _db.Book.FirstOrDefaultAsync(u => u.Id == id);
+            if(bookFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Deleting" });
+            }
+            _db.Book.Remove(bookFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete successful" });
+        }
     }
 }
